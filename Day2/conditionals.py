78e271582f00cb4facbe12afb9993156aa66bbf7
rauliathos <rauliathos@gmail.com>
2022-06-08 11:18:39 +0000

    FIRST commit

---
+++ Day2/conditionals.py
@@ -0,0 +1,18 @@
+mark = int(input("Input the mark: "))
+
+
+if mark > 85:
+    print("Distinction")
+    if 65<= mark <=85:
+        print("Pass")
+    else:
+        print("Fail")
+
+
+if mark > 85:
+    print("Distinction") 
+    if 65<= mark <=85:
+         print("Pass")
+else:
+    print("Fail")
+ 
